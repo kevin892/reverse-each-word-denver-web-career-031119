44fe2dd133100032eff55a73f423802b8bd7a08b
Kevin Ruggiero <kevinbruggiero@gmail.com>
2019-01-22 03:17:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(string)
-  string.split(/ /)
-  string.each do |stuff|
-    stuff.reverse
+  array = string.split(/ /)
+  reverse_array = []
+  array.each do |string|
+    reverse_array.push(string.reverse)
   end
-stuff.join
-end
+  reverse_array.join(" ")
